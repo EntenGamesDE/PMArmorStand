9621a56c3567c48f98db457b669bd76f6be85a7d
Matze <47496465+Matze997@users.noreply.github.com>
2021-12-14 15:54:25 +0100

    API changes for multi protocol support

--- src/muqsit/pmarmorstand/entity/ArmorStandEntity.php
+++ src/muqsit/pmarmorstand/entity/ArmorStandEntity.php
@@ -72,8 +72,8 @@ class ArmorStandEntity extends Living{
 
 	public function setItemInHand(Item $item_in_hand) : void{
 		$this->item_in_hand = $item_in_hand;
-		$packet = MobEquipmentPacket::create($this->getId(), ItemStackWrapper::legacy(TypeConverter::getInstance()->coreItemStackToNet($this->getItemInHand())), 0, 0, ContainerIds::INVENTORY);
 		foreach($this->getViewers() as $viewer){
+            $packet = MobEquipmentPacket::create($this->getId(), ItemStackWrapper::legacy(TypeConverter::getInstance()->coreItemStackToNet($viewer->getNetworkSession()->getProtocolId(), $this->getItemInHand())), 0, 0, ContainerIds::INVENTORY);
 			$viewer->getNetworkSession()->sendDataPacket($packet);
 		}
 	}
@@ -90,7 +90,7 @@ class ArmorStandEntity extends Living{
 
 	protected function sendSpawnPacket(Player $player) : void{
 		parent::sendSpawnPacket($player);
-		$player->getNetworkSession()->sendDataPacket(MobEquipmentPacket::create($this->getId(), ItemStackWrapper::legacy(TypeConverter::getInstance()->coreItemStackToNet($this->getItemInHand())), 0, 0, ContainerIds::INVENTORY));
+		$player->getNetworkSession()->sendDataPacket(MobEquipmentPacket::create($this->getId(), ItemStackWrapper::legacy(TypeConverter::getInstance()->coreItemStackToNet($player->getNetworkSession()->getProtocolId(), $this->getItemInHand())), 0, 0, ContainerIds::INVENTORY));
 	}
 
 	protected function addAttributes() : void{
